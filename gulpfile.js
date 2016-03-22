d72c862dcf84ca00cad5604984e67d886b9dc00c
Daniel Bush <dlb.id.au@gmail.com>
2016-03-23 00:19:36 +1100

    SETUP: made Dockerfile the base image.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 const gulp = require('gulp'),
       spawn = require('child_process').spawn;
 
-const IMAGENAME="ps-kata-agent:0.1",
-      CONTAINER="ps-kata-agent-1"
+const DOCKER = {
+  base: {
+    IMAGENAME: "ps-kata-base",
+    CONTAINER: "ps-kata-base"
+  },
+  agent: {
+    IMAGENAME: "ps-kata-agent:0.1",
+    CONTAINER: "ps-kata-agent-1"
+  }
+}
 
 function printToConsole (process) {
   process.stdout.on('data', (data) => console.log(data.toString()) );
@@ -11,7 +19,7 @@ function printToConsole (process) {
 
 gulp.task('docker:run', ['docker:rm-container', 'docker:build'], function () {
   const cmd = 'cat',
-        dockerCmd = `docker run -id --name=${CONTAINER} ${IMAGENAME} bash -lc "${cmd}"`;
+        dockerCmd = `docker run -id --name=${DOCKER.base.CONTAINER} ${DOCKER.base.IMAGENAME} bash -lc "${cmd}"`;
 
   //console.log(dockerCmd);
 
@@ -20,14 +28,14 @@ gulp.task('docker:run', ['docker:rm-container', 'docker:build'], function () {
 });
 
 gulp.task('docker:rm-container', function (cb) {
-  const dockerCmd = `docker rm -f ${CONTAINER}`;
+  const dockerCmd = `docker rm -f ${DOCKER.base.CONTAINER}`;
   const kill = spawn('sudo', dockerCmd.split(' '));
   kill.on('close', (err) => cb()); // ignore the error here
   printToConsole(kill);
 })
 
 gulp.task('docker:build', function (cb) {
-  const dockerCmd = `docker build -t ${IMAGENAME} .`;
+  const dockerCmd = `docker build -t ${DOCKER.base.IMAGENAME} .`;
   const build = spawn('sudo', dockerCmd.split(' '));
   build.on('close', (err) => cb(err) )
   printToConsole(build);
