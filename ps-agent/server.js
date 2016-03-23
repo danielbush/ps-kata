93d51eed4db530fd1dcd1f0c62d16954d79d31e0
Daniel Bush <dlb.id.au@gmail.com>
2016-03-24 06:11:24 +1100

    FEATURE: ps-agent server script gets port from environment.

--- ps-agent/server.js
+++ ps-agent/server.js
@@ -1,12 +1,13 @@
 /**
- * @fileOverview Run the agent on port 3000.
+ * @fileOverview Run the agent on selected port.
  * @name server.js<ps-agent>
  * @author 
  * @license BSD 2 Clause
  */
 
-const app = require('./app');
+const app = require('./app'),
+      port = process.env.NODE_PORT || 4000;
 
-app.listen(3000);
+app.listen(port);
 
-console.log('Agent listening on 3000');
+console.log(`Agent listening on ${port}`);
